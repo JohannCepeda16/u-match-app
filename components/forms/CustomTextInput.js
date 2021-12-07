14ee800c5d7ed1d78c0f911a66b2756b0bba8a38
David Coronado <david.coronado@escuelaing.edu.co>
2021-12-07 10:20:38 -0500

    added Components CustomTextInput

---
+++ components/forms/CustomTextInput.js
@@ -0,0 +1,26 @@
+import { StyleSheet, TextInput } from "react-native";
+import React from "react";
+
+export default function CustomTextInput(props) {
+  const { placeholder, onChange, value, type } = props;
+  return (
+    <TextInput
+      style={styles.input}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      keyboardType={type}
+      secureTextEntry={type === "visible-password"}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 10,
+    backgroundColor: "#fff",
+    margin: 10,
+    justifyContent: "center",
+    textAlign: "center",
+  },
+});
