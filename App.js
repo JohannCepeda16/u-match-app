310e0b71ea0b5df610d2d5a02021875fd6ef9189
jacepeda <jacepeda@indracompany.com>
2021-12-07 14:30:26 -0500

    App updates

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { colors } from "./constants/index";
+import Login from "./modules/auth/Login";
+import Home from "./modules/home/Home";
+import { firebaseConfig } from "./firebase/firebaseConfig";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Register from "./modules/auth/Register";
+import UserContext from "./persistence/UserContext";
+import { onAuthStateChanged } from "firebase/auth";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [userId, setUserId] = useState();
+
+    useEffect(() => {
+        onAuthStateChanged((user) => {
+            if (user) {
+                setUserId(user.uid);
+            }
+        });
+    }, []);
+
+    return (
+        <UserContext.Provider value={userId}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen component={Login} name="Login" />
+                    <Stack.Screen component={Register} name="Register" />
+                    <Stack.Screen component={Home} name="Home" />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </UserContext.Provider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+        backgroundColor: colors.primary,
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+    },
 });
