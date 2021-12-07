24d8dd5c6b552c01f3b96ecc01310ae7cb542a1a
jacepeda <jacepeda@indracompany.com>
2021-12-07 14:31:11 -0500

    Updates

--- modules/auth/Login.js
+++ modules/auth/Login.js
@@ -1,3 +1,109 @@
-export default function Login() {
-  return <View></View>;
+import React, { useState } from "react";
+import { View, Text, StatusBar, StyleSheet, TextInput } from "react-native";
+import CustomButton from "../../components/forms/CustomButton";
+import CustomTextInput from "../../components/forms/CustomTextInput";
+import { colors } from "../../constants";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import {initializeApp} from "firebase/app";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBm1wyOi7y5GXndue_PbCK54Gwij_ni3U4",
+  authDomain: "umatching-d13c4.firebaseapp.com",
+  projectId: "umatching-d13c4",
+  storageBucket: "umatching-d13c4.appspot.com",
+  messagingSenderId: "115293416241",
+  appId: "1:115293416241:web:bcaf140aa4fd5c684c1e10",
+  measurementId: "G-K6TC866SE5",
+};
+
+initializeApp(firebaseConfig);
+
+
+const auth = getAuth();
+
+export default function Login(props) {
+    const [user, setUser] = useState({ email: "", password: "" });
+	
+    const login = () => {
+        console.log(user);
+        signInWithEmailAndPassword(auth, user.email, user.password)
+            .then((userCredential) => {
+				var user = userCredential.user;
+				console.log("Is logged", user)
+                props.navigation.navigate("Home");
+            })
+            .catch((error) => {
+                if (error.code == "auth/user-not-found") {
+                    alert("Datos no encontrados");
+                } else if (error.code == "auth/too-many-requests") {
+                    alert("Demasiados intentos fallidos, intenta mas tarde");
+                } else if (error.code == "auth/wrong-password") {
+                    alert("Usuario o contraseña incorrectos");
+                }
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log("Error auth", error);
+            });
+    };
+
+    const handleData = (fieldName, value) => {
+        setUser({ ...user, [fieldName]: value });
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={{ color: "white", fontSize: 50 }}>U-MATCHING</Text>
+            <Text style={{ color: "white", fontSize: 18 }}>
+                Encuentra tu pareja perfecta
+            </Text>
+            <StatusBar style="auto" />
+
+            <View style={styles.base}>
+                <Text style={styles.text}>Correo electrónico</Text>
+                <CustomTextInput
+                    placeholder="ejemplo@ejemplo.com"
+                    type="email-address"
+                    onChange={(text) => handleData("email", text)}
+                />
+                <Text style={styles.text}>Contraseña</Text>
+                <CustomTextInput
+                    onChange={(text) => handleData("password", text)}
+                    placeholder="********"
+                    type="visible-password"
+                />
+            </View>
+            <CustomButton title="Ingresar" onClick={() => login()} />
+            <View style={{ marginTop: 20 }}>
+                <Text style={styles.text}>¿No tienes cuenta?</Text>
+                <Text
+                    style={[styles.text, { color: colors.secondary }]}
+                    onPress={() => props.navigation.navigate("Register")}
+                >
+                    Registrarse
+                </Text>
+            </View>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.primary,
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+    },
+    base: {
+        backgroundColor: colors.secondary,
+        padding: 20,
+        borderRadius: 10,
+        width: "80%",
+        marginTop: 10,
+    },
+    text: {
+        color: "white",
+        fontSize: 15,
+        textAlign: "center",
+    },
+});
--- modules/auth/Register.js
+++ modules/auth/Register.js
@@ -1,7 +1,139 @@
-export default function Register(){
-    return(
-        <View>
-            
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { colors } from "../../constants";
+import CustomTextInput from "../../components/forms/CustomTextInput";
+import CustomButton from "../../components/forms/CustomButton";
+import UserServices from "../../services/UserServices";
+
+export default function Register(props) {
+    const [user, setUser] = useState({
+        program: "Ingeniería de Sistemas",
+        gender: "Hombre",
+        college: "Escuela Colombiana de Ingeniería Julio Garavito",
+    });
+    const [validPsw, setValidPsw] = useState("");
+    const [step, setStep] = useState(0);
+
+    function register(e) {
+        // e.preventDefault();
+        //COMPROBAR LA DIRECCION DE CORREO ELECTRONICO
+        if (step == 0) {
+            console.log(user);
+            UserServices.findUserByEmail(user.email)
+                .then((res) => {
+                    if (res) res.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    if (data) {
+                        alert("El email registrado ya se encuentra en uso");
+                    } else {
+                        setStep(1);
+                    }
+                })
+                .catch((error) => console.log(error));
+
+            //ENVIAR VERIFICACION DE CORREO ELECTRONICO
+        } else if (step == 1) {
+            //userRef.sendEmailVerification();
+            setStep(2);
+            //CREAR USUARIO CON EMAIL && PASSWORD
+        } else if (step == 2) {
+            if (user.password == validPsw) {
+                firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(user.email, user.password)
+                    .then((userCredential) => {
+                        var userRef = userCredential.user;
+                        let finalUser = user;
+                        finalUser.uid = userRef.uid;
+                        UserServices.createUser(finalUser)
+                            .then(() => {
+                                history.push("/profile");
+                            })
+                            .catch((error) => console.log(error));
+                    })
+                    .catch((error) => console.log(error));
+            } else {
+                alert("Las contraseñas deben coincidir");
+            }
+        }
+    }
+
+    const handleData = (e) => {
+        // e.preventDefault();
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={{ color: "white", fontSize: 50 }}>U-MATCHING</Text>
+            <Text style={{ color: "white", fontSize: 18 }}>
+                Encuentra tu pareja perfecta
+            </Text>
+            <View style={styles.base}>
+                {step === 0 && (
+                    <>
+                        <Text style={styles.text}>Nombre y apellido</Text>
+                        <CustomTextInput placeholder="Pepito Perez" />
+                        <Text style={styles.text}>Universidad</Text>
+                        <CustomTextInput placeholder="Pepito Perez" />
+                        <Text style={styles.text}>Programa académico</Text>
+                        <CustomTextInput placeholder="Pepito Perez" />
+                        <Text style={styles.text}>Semestre</Text>
+                        <CustomTextInput placeholder="Pepito Perez" />
+                        <Text style={styles.text}>Correo electrónico</Text>
+                        <CustomTextInput
+                            placeholder="nombre.apellido@mail..."
+                            type="email-address"
+                        />
+                    </>
+                )}
+                {step === 1 && (
+                    <>
+                        <Text style={styles.text}>Edad</Text>
+                        <CustomTextInput placeholder="Pepito Perez" />
+                        <Text style={styles.text}>Genero</Text>
+                        <CustomTextInput placeholder="Seleccione..." />
+                        <Text style={styles.text}>Instagram (opcional)</Text>
+                        <CustomTextInput placeholder="@username" />
+                        <Text style={styles.text}>Whatsapp (opcional)</Text>
+                        <CustomTextInput placeholder="310 555555" />
+                    </>
+                )}
+            </View>
+            <CustomButton title="Continuar" onClick={() => register()} />
+            <View style={{ marginTop: 20 }}>
+                <Text style={styles.text}>¿Ya tienes cuenta?</Text>
+                <Text
+                    style={[styles.text, { color: colors.secondary }]}
+                    onPress={() => props.navigation.navigate("Login")}
+                >
+                    Iniciar sersión
+                </Text>
+            </View>
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.primary,
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+    },
+    base: {
+        backgroundColor: colors.secondary,
+        padding: 20,
+        borderRadius: 10,
+        width: "80%",
+        marginTop: 10,
+    },
+    text: {
+        color: "white",
+        fontSize: 15,
+        textAlign: "center",
+    },
+});
