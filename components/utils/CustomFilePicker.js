01aa7ab85999cce87839399f7bf455eea1513e14
MiguelFuquene1024 <migueplay_10@hotmail.com>
2021-12-09 09:41:51 -0500

    Bug with selected file

--- components/utils/CustomFilePicker.js
+++ components/utils/CustomFilePicker.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, View, StyleSheet, Image } from "react-native";
 import { colors } from "../../constants";
 import * as ImagePicker from "expo-image-picker";
 import CustomButton from "../forms/CustomButton";
 
 export default function CustomFilePicker(props) {
-    const { handleFile } = props;
+    const { handleFile, value } = props;
 
     const [file, setFile] = useState();
+
+    useEffect(() => {
+        console.log("File received", value);
+    }, [value]);
 
     let openImagePickerAsync = async () => {
         let permissionResult =
@@ -19,18 +23,22 @@ export default function CustomFilePicker(props) {
         }
 
         let pickerResult = await ImagePicker.launchImageLibraryAsync();
+        console.log("Image", pickerResult);
         setFile(pickerResult);
-        handleFile(pickerResult);
+        handleFile(pickerResult.uri, new Date().toISOString());
     };
 
     return (
         <View style={styles.card}>
-            {file ? (
-                <Image style={styles.picture} source={file} />
+            {value || file ? (
+                <Image
+                    style={styles.picture}
+                    source={value ? { uri: value } : file}
+                />
             ) : (
                 <CustomButton
                     title="Seleccionar"
-                    onPress={openImagePickerAsync}
+                    onClick={openImagePickerAsync}
                 ></CustomButton>
             )}
         </View>
@@ -44,16 +52,16 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         height: 200,
-        width:200,
+        width: 200,
         margin: 5,
     },
-    picture:{
-        width:200,
-        height:200
+    picture: {
+        width: 200,
+        height: 200,
     },
     button: {
         marginTop: "auto",
         color: colors.secondary,
-        alignSelf:"center"
+        alignSelf: "center",
     },
 });
