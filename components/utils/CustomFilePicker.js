6915bfbe2c19a9827d81b17b36f578839f35de15
MiguelFuquene1024 <migueplay_10@hotmail.com>
2021-12-09 09:38:18 -0500

    customfilepicker config

--- components/utils/CustomFilePicker.js
+++ components/utils/CustomFilePicker.js
@@ -1,48 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { Button, View, StyleSheet, Image } from "react-native";
+import React from "react";
+import { Button, Pressable, StyleSheet } from "react-native";
 import { colors } from "../../constants";
 import * as ImagePicker from "expo-image-picker";
-import CustomButton from "../forms/CustomButton";
 
 export default function CustomFilePicker(props) {
-	
-    const { handleFile, value } = props;
-
-    const [file, setFile] = useState();
-
-    useEffect(() => {
-        console.log("File received", value);
-    }, [value]);
+    const { handleFile } = props;
 
     let openImagePickerAsync = async () => {
         let permissionResult =
             await ImagePicker.requestMediaLibraryPermissionsAsync();
 
         if (permissionResult.granted === false) {
-            alert("El acesso a la camara es requerido");
+            alert("Permission to access camera roll is required!");
             return;
         }
 
         let pickerResult = await ImagePicker.launchImageLibraryAsync();
-		
-        console.log("Image", pickerResult);
-        setFile(pickerResult);
-        handleFile(pickerResult.uri, new Date().toISOString());
+        handleFile(pickerResult);
     };
 
     return (
         <View style={styles.card}>
-            {value || file ? (
-                <Image
-                    style={styles.picture}
-                    source={value ? { uri: value } : file}
-                />
-            ) : (
-                <CustomButton
-                    title="Seleccionar"
-                    onClick={openImagePickerAsync}
-                ></CustomButton>
-            )}
+            <Button title="Seleccionar" onPress={openImagePickerAsync}></Button>
         </View>
     );
 }
@@ -53,17 +32,5 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         justifyContent: "center",
         alignItems: "center",
-        height: 200,
-        width: 200,
-        margin: 5,
-    },
-    picture: {
-        width: 200,
-        height: 200,
-    },
-    button: {
-        marginTop: "auto",
-        color: colors.secondary,
-        alignSelf: "center",
     },
 });
