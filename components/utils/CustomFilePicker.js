fab239c7d7dd9a6fe4b08918c0ec82cd9b822263
MiguelFuquene1024 <migueplay_10@hotmail.com>
2021-12-09 09:39:22 -0500

    Update

--- components/utils/CustomFilePicker.js
+++ components/utils/CustomFilePicker.js
@@ -1,10 +1,12 @@
-import React from "react";
-import { Button, Pressable, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Button, View, StyleSheet, Image } from "react-native";
 import { colors } from "../../constants";
 import * as ImagePicker from "expo-image-picker";
 
 export default function CustomFilePicker(props) {
     const { handleFile } = props;
+
+    const [file, setFile] = useState();
 
     let openImagePickerAsync = async () => {
         let permissionResult =
@@ -16,12 +18,21 @@ export default function CustomFilePicker(props) {
         }
 
         let pickerResult = await ImagePicker.launchImageLibraryAsync();
+        setFile(pickerResult);
         handleFile(pickerResult);
     };
 
     return (
         <View style={styles.card}>
-            <Button title="Seleccionar" onPress={openImagePickerAsync}></Button>
+            {file ? (
+                <Image style={styles.picture} source={file} />
+            ) : (
+                <Button
+                    style={styles.button}
+                    title="Seleccionar"
+                    onPress={openImagePickerAsync}
+                ></Button>
+            )}
         </View>
     );
 }
@@ -32,5 +43,17 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         justifyContent: "center",
         alignItems: "center",
+        height: 200,
+        width:200,
+        margin: 5,
+    },
+    picture:{
+        width:200,
+        height:200
+    },
+    button: {
+        marginTop: "auto",
+        color: colors.secondary,
+        alignSelf:"center"
     },
 });
